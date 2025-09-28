98a513853d697e028c41a03816e3143bb1bfae16
krazyorange13 <77130315+krazyorange13@users.noreply.github.com>
2025-09-28 13:10:36 -0500

    run random chess game

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+import torch
+import chess
+
+
+class ChessGame:
+    def __init__(self):
+        self.board = chess.Board()
+
+    def run(self):
+        while not (
+            self.board.is_checkmate()
+            or self.board.is_game_over()
+            or self.board.can_claim_draw()
+            or self.board.can_claim_fifty_moves()
+            or self.board.can_claim_threefold_repetition()
+            # idk what these do
+            # or self.board.is_variant_draw()
+            # or self.board.is_variant_end()
+            # or self.board.is_variant_loss()
+            # or self.board.is_variant_win()
+        ):
+            moves = list(self.board.generate_legal_moves())
+            if not moves:
+                return
+            move = random.choice(moves)
+            self.board.push(move)
+            print(move)
+            print(self.board)
+            print("\n")
+        print("result:", self.board.result())
+
+
+if __name__ == "__main__":
+    game = ChessGame()
+    game.run()
