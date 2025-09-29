27ae50d2002e76370b60668ed550690b3477eb9e
mattseq <matthewsequeir@gmail.com>
2025-09-28 20:17:28 -0500

    tried to integrate the agent into the game. not working :(

--- agent.py
+++ agent.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import random
 
 class Agent:
-    def __init__(self):
+    def __init__(self, color=chess.WHITE):
         self.board = chess.Board()
         self.model = nn.Sequential(
             nn.Linear(64, 128),
@@ -13,6 +13,7 @@ class Agent:
             nn.ReLU(),
             nn.Linear(64, 1)
         )
+        self.color = color
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
         self.criterion = nn.MSELoss()
         self.epsilon = 0.1
@@ -37,7 +38,7 @@ class Agent:
                 value = piece_to_value[piece.piece_type]
 
                 # Negative for black pieces (or enemy pieces later on)
-                if piece.color == chess.BLACK:
+                if piece.color != self.color:
                     value = -value
                 
                 board_tensor[square] = value
@@ -46,7 +47,7 @@ class Agent:
     
     def get_best_move_and_val(self):
         # get legal moves
-        legal_moves = self.board.legal_moves
+        legal_moves = list(self.board.legal_moves)
         if not legal_moves:
             return None
         
@@ -78,8 +79,9 @@ class Agent:
     
 
     
-    def train_step(self, move):
+    def train_step(self, move, value):
         # get tensor of old board
+        old_board = self.board
         old_board_state = self.board_to_tensor(self.board)
 
         # make move
@@ -89,12 +91,12 @@ class Agent:
         new_board_state = self.board_to_tensor(self.board)
 
         # calculate reward
-        reward = self.getReward(old_board_state, new_board_state, move)
+        reward = self.getReward(old_board, self.board, move)
         
         # the old prediction for the previous state should be updated based on reward
         # Q-learning update algorithm: Q(s, a) = Q(s, a) + alpha * (reward + discount * max_next_Q - Q(s, a))
         predicted_q = self.model.forward(old_board_state)
-        target_q = reward + self.discount * self.get_best_move_and_val()[1]
+        target_q = reward + self.discount * value
 
         # Update model
         self.optimizer.zero_grad()
@@ -102,6 +104,16 @@ class Agent:
         loss.backward()
         self.optimizer.step()
 
+        return move
+
     def getReward(self, old_state, new_state, move):
-        # TODO: implement reward function
-        return 0
+        reward = 0
+
+        if (new_state.is_checkmate()):
+            reward += 100
+        elif (new_state.is_stalemate() or new_state.is_insufficient_material()
+                or new_state.can_claim_draw() or new_state.can_claim_fifty_moves()
+                or new_state.can_claim_threefold_repetition()):
+                reward += 10
+
+        return reward
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import random
 import torch
 import chess
-
+from agent import Agent
 
 class ChessGame:
     def __init__(self):
         self.board = chess.Board()
+        self.agent1 = Agent(color=chess.WHITE)
+        self.agent2 = Agent(color=chess.BLACK)
 
     def run(self):
         while not (
@@ -20,12 +22,31 @@ class ChessGame:
             # or self.board.is_variant_loss()
             # or self.board.is_variant_win()
         ):
-            moves = list(self.board.generate_legal_moves())
-            if not moves:
+            
+            # agent 1 move
+            move, val =self.agent1.get_best_move_and_val()
+            agent1_move = self.agent1.train_step(move, val)
+
+            # update main board
+            self.board.push(agent1_move)
+            # update agents boards
+            self.agent1.board.push(agent1_move)
+            self.agent2.board.push(agent1_move)
+
+            # check if game over after agent 1 move
+            if self.board.is_game_over():
                 return
-            move = random.choice(moves)
-            self.board.push(move)
-            print(move)
+            
+            # agent 2 move
+            agent2_move = self.agent2.get_best_move_and_val()[0]
+            self.board.push(agent2_move)
+            
+            # update main board
+            self.board.push(agent1_move)
+            # update agents boards
+            self.agent1.board.push(agent2_move)
+            self.agent2.board.push(agent2_move)
+
             print(self.board)
             print("\n")
             input("Press Enter to continue...")
